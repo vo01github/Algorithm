9d38413ece88c0f405e651b270536392b7a13b85
vo01github <hxt2014bbg@icloud.com>
2015-03-21 15:21:32 +0800

    1

---
+++ 01-蓄势待发——准备篇.cpp
@@ -0,0 +1,31 @@
+﻿
+
+// 第01章 蓄势待发——准备篇
+/*
+	
+*/
+// __________________________1.2 最负盛名的程序设计竞赛__________________________
+
+// 1.2.1  	Google Code Jam (GCJ)		http://code.google.com/codejam/
+// 1.2.2  	TopCoder
+// 1.2.3 	ACM (大学生)
+// 1.2.4	IOI(初中，高中)
+/* 1.2.5 	Online Judge(OJ)
+	
+	PKU Online Judge(POJ) 			http://poj.org
+	题库中有大量的题目
+
+	Sphere Online Judge(SPOJ)		http://www.spoj.pl/
+	允许使用各种各样的编程语言
+
+	SGU Online Contester			http://acm.sgu.ru/
+	具有模拟参加历史比赛的虚拟赛功能
+
+	Codeforces						http://codeforces.com
+	不断维护历届题库
+*/ 
+
+
+
+// __________________________1.4 如何提交解答__________________________
+
