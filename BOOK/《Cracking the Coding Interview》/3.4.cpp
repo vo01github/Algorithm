0fcdfa9e764c152c75bc9d21b545da0d76d655df
vo01github <hxt2014bbg@icloud.com>
2015-05-30 17:53:40 +0800

    1

---
+++ BOOK/《Cracking the Coding Interview》/3.4.cpp
@@ -0,0 +1,22 @@
+﻿
+
+// 8.3 栈与队列
+
+
+// __________________________ 3.4 __________________________
+/*
+原文：
+
+In the classic problem of the Towers of Hanoi, you have 3 rods and N disks of different sizes which can slide onto any tower. The puzzle starts with disks sorted in ascending order of size from top to bottom (e.g., each disk sits on top of an even larger one). You have the following constraints:
+
+Only one disk can be moved at a time.
+A disk is slid off the top of one rod onto the next rod.
+A disk can only be placed on top of a larger disk.
+Write a program to move the disks from the first rod to the last using Stacks
+
+译文：
+
+编程解决汉诺塔问题，使用数据结构栈
+*/
+
+
